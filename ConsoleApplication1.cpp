1ca895a4ca9e20da7b117d886d4ede92fed588d5
m-holovan <nikita2003n@gmail.com>
2023-09-08 18:59:14 +0300

    Laba 0

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,9 +1,18 @@
-﻿// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿#include <iostream>
+#include <limits>
+#include<locale.h>
 
-#include <iostream>
+int main() {
+	setlocale(LC_CTYPE, "rus");
 
-int main()
-{
-    std::cout << "Hello World!\n";
+	std::cout << "Тип данных\tРазмер (байт)\tМинимальное значение\tМаксимальное значение" << std::endl;
+
+	std::cout << "int\t\t" << sizeof(int) << "\t\t" << std::numeric_limits<int>::min() << "\t\t" << std::numeric_limits<int>::max() << std::endl;
+	std::cout << "short\t\t" << sizeof(short) << "\t\t" << std::numeric_limits<short>::min() << "\t\t\t" << std::numeric_limits<short>::max() << std::endl;
+	std::cout << "long\t\t" << sizeof(long) << "\t\t" << std::numeric_limits<long>::min() << "\t\t" << std::numeric_limits<long>::max() << std::endl;
+	std::cout << "char\t\t" << sizeof(char) << "\t\t" << static_cast<int>(std::numeric_limits<char>::min()) << "\t\t\t" << static_cast<int>(std::numeric_limits<char>::max()) << std::endl;
+	std::cout << "float\t\t" << sizeof(float) << "\t\t" << std::numeric_limits<float>::lowest() << "\t\t" << std::numeric_limits<float>::max() << std::endl;
+	std::cout << "double\t\t" << sizeof(double) << "\t\t" << std::numeric_limits<double>::lowest() << "\t\t" << std::numeric_limits<double>::max() << std::endl;
+
+	return 0;
 }
